275e018eec98f70df74035cf85e06b7d9d9d2dbd
Thrijwal K <thrijwalk@gmail.com>
2026-08-26 13:42:40 +0100

    modified the logging and added the mutation and clean up files

---
+++ CelllineSelector/ml_build/logger.py
@@ -0,0 +1,22 @@
+import logging,yaml,os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+def get_logger(name):
+
+    logfilepath = "logs"
+    logfile= "machinelearning.log"
+    log_file = os.path.join(BASE_DIR,logfilepath,logfile)
+    os.makedirs(os.path.dirname(log_file),
+                exist_ok=True)
+    logger=logging.getLogger(name)
+    logger.setLevel('INFO')
+    formatter=logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
+    fh=logging.FileHandler(log_file)
+    sh=logging.StreamHandler()
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+
+    return logger
